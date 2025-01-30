b767c35b270357e41340f0d112326d9afffe0a04
dachieng <oloodorcas99@gmail.com>
2025-01-30 22:38:25 +0300

    feat: add to card from products page

--- src/app/features/products/components/product-grid/product-grid.component.ts
+++ src/app/features/products/components/product-grid/product-grid.component.ts
@@ -2,16 +2,34 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
-import { Product } from '../../../../core/services/product/product.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { Product } from '../../../../core/services/product/product.service';
+import { CartService } from '../../../../core/services/cart/cart.service';
 
 @Component({
   selector: 'app-product-grid',
   standalone: true,
-  imports: [CommonModule, MatGridListModule, MatCardModule, RouterLink],
+  imports: [
+    CommonModule, 
+    MatGridListModule, 
+    MatCardModule, 
+    RouterLink, 
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './product-grid.component.html',
   styleUrls: ['./product-grid.component.scss'],
 })
 export class ProductGridComponent {
   @Input() products: Product[] = [];
+
+  constructor(private cartService: CartService) {}
+
+  addToCart(event: Event, product: Product): void {
+    event.preventDefault(); // Prevent navigation
+    event.stopPropagation(); // Prevent event bubbling
+    this.cartService.addToCart(product, 1);
+  }
 }
