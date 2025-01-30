133e248bb8ea8eb10dc7c2904e247dfa8e2b70ef
dachieng <oloodorcas99@gmail.com>
2025-01-30 23:11:00 +0300

    feat: style footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet, FooterComponent],
+  template: `
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+    <app-footer></app-footer>
+  `,
 })
 export class AppComponent {
   title = 'e-commerce';
