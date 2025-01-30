ace34694e21c76b16c8d2cec8ca4bce17ee6d996
dachieng <oloodorcas99@gmail.com>
2025-01-30 23:05:50 +0300

    add footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatIconModule, RouterLink],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  navigationItems = [
+    { icon: 'home', label: 'HOME', link: '/' },
+    { icon: 'category', label: 'CATEGORIES', link: '/categories' },
+    { icon: 'shopping_cart', label: 'MY CART', link: '/cart' },
+  ];
 }
