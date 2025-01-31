1411ce9c20432012b3b6151ced5d27f003d59865
dachieng <oloodorcas99@gmail.com>
2025-01-31 06:53:03 +0300

    feat: add categories list

--- src/app/core/services/category/category.service.ts
+++ src/app/core/services/category/category.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { Observable, map } from 'rxjs';
+import { GET_ALL_CATEGORIES } from '../../graphql/queries';
+
+export interface Category {
+  id: string;
+  name: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  constructor(private apollo: Apollo) { }
 
-  constructor() { }
+  getCategories(): Observable<Category[]> {
+    return this.apollo
+      .watchQuery<{ categories: Category[] }>({
+        query: GET_ALL_CATEGORIES,
+      })
+      .valueChanges.pipe(map((result) => result.data.categories));
+  }
 }
--- src/app/features/categories/categories/categories.component.ts
+++ src/app/features/categories/categories/categories.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { CategoryService, Category } from '../../../core/services/category/category.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.scss'
+  styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categories$: Observable<Category[]>;
 
+  constructor(private categoryService: CategoryService) {
+    this.categories$ = this.categoryService.getCategories();
+  }
+
+  ngOnInit(): void {}
 }
